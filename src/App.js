1cb220d31ffb6e9c983e65200d8761420e7cd735
niketanmoon <niketanmoon@gmail.com>
2022-05-29 22:56:10 +0930

    Added sections like primary promotions and social

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import Mail from "./components/Mail";
+import EmailList from "./components/EmailList";
 
 function App() {
   return (
     <div className="app">
       <Header />
-
-      <Sidebar />
+      <div className="app__body">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<EmailList />} />
+          <Route path="mail" element={<Mail />} />
+        </Routes>
+      </div>
     </div>
   );
 }
